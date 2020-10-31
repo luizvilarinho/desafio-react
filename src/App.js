4c48ea62f98990410684bc6efea3c44448a7eb55
luizvilarinho <luizvilarinho@gmail.com>
2020-10-31 15:53:06 -0300

    chore:desafio completo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,65 @@
-import React from "react";
-
+import React, { useState, useEffect, useRef } from "react";
+import api from './services/api.js'
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([]);
+	var title = useRef(title);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	useEffect(() => {
+		api.get('/repositories').then(response => {
+			console.log(response.data);
+			setRepositories(response.data);
+		})
+	}, []);
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+	async function handleAddRepository() {
+		// TODO
+		let newRepositorie = {
+			title:title.current.value,
+			url:"https://github.com/react",
+			techs:["react"]
+		}
+
+		let response = await api.post('/repositories', newRepositorie);
+
+		setRepositories([...repositories, response.data]);
+	}
+
+	async function handleRemoveRepository(id) {
+		// TODO
+		await api.delete(`/repositories/${id}`);
+
+    let newRepositories = repositories.filter(rep=> rep.id !=  id);
+
+    setRepositories(newRepositories);
+
+	/*	api.get('/repositories').then(response=>{
+			setRepositories(response.data);
+    })
+    */
+	}
+
+
+	return (
+		<div>
+			<ul data-testid="repository-list">
+				{repositories.map(rep =>
+					<li key={rep.id}>
+						<h3>{rep.title}</h3>
+						<button onClick={() => handleRemoveRepository(rep.id)}>
+							Remover
+                    </button>
+					</li>
+				)}
+			</ul>
+
+			<input type="text" name="title" ref={title} placeholder="title" />
+			<button onClick={handleAddRepository}>Adicionar</button>
+		</div>
+	);
 }
 
 export default App;
